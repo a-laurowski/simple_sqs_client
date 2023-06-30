ef4f3427fe4e4f5cdf34b8f38dba876ce6ff1f6b
alaurowski <aleksander.laurowski1991@gmail.com>
2023-06-30 09:29:06 +0200

    fix wrong service name
    fix wrong service name

--- src/simple_sqs_client/setup.py
+++ src/simple_sqs_client/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simple_sqs_client',
-    version='0.9.3',
+    version='0.9.4',
     author='Aleksander Laurowski',
     author_email='aleksander.laurowski1991@gmail.com',
     description='A very simple package for interacting with SQS (Simple Queue Service) in AWS',
