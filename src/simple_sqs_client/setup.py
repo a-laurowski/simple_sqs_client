87699a330ccf890eb96c50ee681e8304a23daa18
alaurowski <aleksander.laurowski1991@gmail.com>
2023-08-24 15:09:10 +0200

    change version

--- src/simple_sqs_client/setup.py
+++ src/simple_sqs_client/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simple_sqs_client',
-    version='0.9.5',
+    version='0.9.6',
     author='Aleksander Laurowski',
     author_email='aleksander.laurowski1991@gmail.com',
     description='A very simple package for interacting with SQS (Simple Queue Service) in AWS',
